1c4c855843abd25e1b51e94d3b150afea384ccd4
Noa Prevel <73242859+Lokisn@users.noreply.github.com>
2021-04-06 16:32:11 -0400

    Mise en commentaire de contenu + Ajout de contenu test pour l'essai de la mise en page

--- feed-project.php
+++ feed-project.php
@@ -4,23 +4,33 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/style.css">
     <title>RS</title>
 </head>
 <body>
     <!-- intégrer ici l'entête/ la barre de recherche/ etc. -->
-
-
+    <?php
+        include "header.php";
+    ?>
 
     <?php 
-        include "db.php";
+        /*include "db.php";
         $request = "SELECT `id_projet`, `nom`, `type_media`, `url_media` FROM `projet`";
         $sth = $dbh->prepare($request);
         $sth->execute();
         $projects = $sth->fetchAll();
         function writeMsg() {
             echo "Hello world!";
-          }
-        foreach($projects as $project){
+          }*/
+          ?>
+                <div class="youtube-responsive">
+                    <iframe width="1280" height="720" src="" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+                <div class="conteneur-vignette">
+                    <img class="media-fluide" src="https://images.unsplash.com/photo-1481349518771-20055b2a7b24?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1109&q=80" alt="image représentant le projet">
+                </div>
+          <?php
+        /*foreach($projects as $project){
             ?>
             <div class="projet-card">
                     <?php 
@@ -46,7 +56,11 @@
                     <p class="nb-like" onclick=""><?=$project['likes'];?></p>
                 </div>
             <?php
-        }
+        }*/
+    ?>
+    <!-- intégrer ici le footer -->
+    <?php
+        include "footer.php";
     ?>
 </body>
 </html>
