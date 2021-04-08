0bbc4197eedb7f85bb339fc9667198e661dff35c
Noa Prevel <73242859+Lokisn@users.noreply.github.com>
2021-04-08 14:51:03 -0400

    Correction du line vidéo youtube + Modification du CSS de la page Feed

--- feed-project.php
+++ feed-project.php
@@ -25,8 +25,11 @@
           ?>
           <section class="content">
               <div class="youtube-responsive">
-                  <iframe width="1280" height="720" src="https://www.youtube.com/watch?v=dQw4w9WgXcQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                  <!-- NE PAS OUBLIER LE EMBED DANS LE LIEN -->
+                  <!-- NE PAS METTRE DE "&MUTE@" DANS LE LIEN SRC -->
+                  <iframe width="1280" height="720" src="https://www.youtube.com/embed/QAA15b_CdRg"  frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen ></iframe>
               </div>
+
               <div class="conteneur-vignette">
                   <img class="media-fluide" src="https://images.unsplash.com/photo-1481349518771-20055b2a7b24?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1109&q=80" alt="image représentant le projet">
               </div>
