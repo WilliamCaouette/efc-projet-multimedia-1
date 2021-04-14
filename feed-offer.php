b1a212432c09c09e87b769c5a9e23b547d95eaff
Noa Prevel <suplokier@gmail.com>
2021-04-14 16:18:42 +0200

    Création d'un fichier php pour le feed des offres

---
+++ feed-offer.php
@@ -0,0 +1,42 @@
+<?php 
+        include "db.php";
+    ?>
+     
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- CSS -->
+    <link rel="stylesheet" href="style/style.css">
+    <!-- Mustache -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/mustache.js/4.1.0/mustache.min.js" integrity="sha512-HYiNpwSxYuji84SQbCU5m9kHEsRqwWypXgJMBtbRSumlx1iBB6QaxgEBZHSHEGM+fKyCX/3Kb5V5jeVXm0OglQ==" crossorigin="anonymous"></script>
+    <!-- JS -->
+    <script src="scripts/feed-offer.js" defer></script>
+    <title>Eniwan</title>
+</head>
+<body>
+    <!-- intégrer ici l'entête/ la barre de recherche/ etc. -->
+    <?php
+        include "header.php";
+    ?>
+
+    <header>
+        <h2>Offres</h2>
+    </header>
+
+    <main>
+        <section id="js-feed-offer">
+
+        </section>
+    </main>
+
+    <!--Template mustache mettre dans un views--> 
+    <!----> 
+       
+    <?php
+        include "footer.php";
+    ?>
+</body>
+</html>
