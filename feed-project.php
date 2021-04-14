874785bcb81353e56e09dc7fe63384979cf69d61
Noa Prevel <suplokier@gmail.com>
2021-04-14 14:56:18 +0200

    Suppression de la moitié du php, début du html Mustache Template

--- feed-project.php
+++ feed-project.php
@@ -1,3 +1,7 @@
+ <?php 
+        include "db.php";
+    ?>
+    <!--Faire un header appart sur une page html appart--> 
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -13,54 +17,8 @@
         include "header.php";
     ?>
 
-    <?php 
-        include "db.php";
-        $request = "SELECT `id_projet`, `nom`, `type_media`, `url_media` FROM `projet`";
-        $sth = $dbh->prepare($request);
-        $sth->execute();
-        $projects = $sth->fetchAll();
-          ?>
-          <section class="content">
-              <div class="youtube-responsive">
-                  <!-- NE PAS OUBLIER LE EMBED DANS LE LIEN -->
-                  <!-- NE PAS METTRE DE "&MUTE@" DANS LE LIEN SRC -->
-                  <iframe width="1280" height="720" src="https://www.youtube.com/embed/QAA15b_CdRg"  frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen ></iframe>
-              </div>
-
-              <div class="conteneur-vignette">
-                  <img class="media-fluide" src="https://images.unsplash.com/photo-1481349518771-20055b2a7b24?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1109&q=80" alt="image représentant le projet">
-              </div>
-          </section>
-          <?php
-        foreach($projects as $project){
-            ?>
-            <div class="projet-card">
-                    <?php 
-                        if($project['type_media'] === 'youtube'){
-                            ?>
-                                <div class="youtube-responsive">
-                                    <iframe width="1280" height="720" src="<?=$project['url_media'];?>" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                                </div>
-                            <?php
-                        }
-                        else{
-                            ?>
-                                <div class="conteneur-vignette">
-                                    <img class="media-fluide" src="<?=$project['url_media'];?>" alt="image représentant le projet">
-                                </div>
-                            <?php
-                        }
-                    ?>
-                    <h3><?=$project['nom'];?></h3>
-                    <a href="detail-projet.php?id_projet=<?=$project['id_projet'];?>">en savoire plus -></a>
-                    <!-- créé la section Like -->
-                    <button class="btn-like">Like</button>
-                    <p class="nb-like" onclick=""><?=$project['likes'];?></p>
-                </div>
-            <?php
-        }
-    ?>
-    <!-- intégrer ici le footer -->
+<!--Commencer HTML MUSTACHE TEMPLATE--> 
+       
     <?php
         include "footer.php";
     ?>
