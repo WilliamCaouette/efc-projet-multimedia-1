2ee557c549da09a15e89859ae6e45946a913dad1
Noa Prevel <suplokier@gmail.com>
2021-04-17 20:08:31 +0200

    Suppression du header.php

--- header.php
+++ header.php
@@ -1,12 +1,1 @@
-<header>
-    <a href="feed-project.php">
-        <h1>Eniwan</h1>
-    </a>
-    <!-- Insérer un btn burger -->
-    <nav>
-        <div>
-            <a href=""></a>
-            <a href=""></a>
-        </div>
-    </nav>
-</header>
+
