910ef795a2d15f0d068ff36c4ee78e2d04d89ed5
Noa Prevel <suplokier@gmail.com>
2021-04-29 08:49:37 +0200

    Suppression des commentaires inutiles

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -1,19 +1,14 @@
 /**
- * Scroll
+ * Scroll à partir d'un bouton dans la page
  *
- * @summary short description for the file
  * @author N.Prevel
  *
  * Created at     : 2021-04-25 19:47:46
- * Last modified  : 2021-04-29 08:43:10
+ * Last modified  : 2021-04-29 08:47:36
  */
-
-// Le nav se situe dans le header.php
 
 const nav = document.querySelector("nav");
 const btnScroll = document.querySelector("#js-btn-scroll");
-
-// remplacer windows par le bouton, pour que le clic force la page à remonter
 
 btnScroll.addEventListener("scroll", function () {
   nav.classList.toggle("sticky", window.scrollY > 0);
