4bff1db106d9d66b6481f54e643238a26b20e698
Noa Prevel <suplokier@gmail.com>
2021-04-29 08:44:08 +0200

    Correction du nom de la navigation (renommer parce que navigation apparaissait deux fois)

--- scripts/menu-burger.js
+++ scripts/menu-burger.js
@@ -3,7 +3,7 @@
  * @author N.Prevel
  *
  * Created at     : 2021-04-25 19:32:12
- * Last modified  : 2021-04-25 19:45:23
+ * Last modified  : 2021-04-29 08:43:17
  */
 
 /*-- Menu burger --*/
@@ -13,18 +13,18 @@ const boxes = document.querySelectorAll(".box");
 window.addEventListener("scroll", checkBoxesAppear);
 
 function checkBoxesAppear() {
-    boxes.forEach((box) => {
-        const boxTop = box.getBoundingClientRect().top;
-        const boxBottom = box.getBoundingClientRect().bottom;
+  boxes.forEach((box) => {
+    const boxTop = box.getBoundingClientRect().top;
+    const boxBottom = box.getBoundingClientRect().bottom;
 
-        const triggerPos = window.innerHeight - (boxBottom - boxTop) / 2;
+    const triggerPos = window.innerHeight - (boxBottom - boxTop) / 2;
 
-        if (boxTop < triggerPos) {
-            box.classList.add("show-box");
-        } else {
-            box.classList.remove("show-box");
-        }
-    });
+    if (boxTop < triggerPos) {
+      box.classList.add("show-box");
+    } else {
+      box.classList.remove("show-box");
+    }
+  });
 }
 
 checkBoxesAppear();
@@ -37,11 +37,11 @@ const navigation = document.querySelector("nav");
 const mainContent = document.querySelector(".main-content");
 
 btnBurger.addEventListener("click", (evt) => {
-    navigation.classList.add("nav-appear");
-    navigation.style.display = "block";
+  navigation.classList.add("nav-appear");
+  navigation.style.display = "block";
 });
 
 btnCancel.addEventListener("click", (evt) => {
-    navigation.classList.remove("nav-appear");
-    navigation.style.display = "none";
+  navigation.classList.remove("nav-appear");
+  navigation.style.display = "none";
 });
--- scripts/scroll.js
+++ scripts/scroll.js
@@ -5,18 +5,18 @@
  * @author N.Prevel
  *
  * Created at     : 2021-04-25 19:47:46
- * Last modified  : 2021-04-29 08:32:22
+ * Last modified  : 2021-04-29 08:43:10
  */
 
 // Le nav se situe dans le header.php
 
-const navigation = document.querySelector("nav");
+const nav = document.querySelector("nav");
 const btnScroll = document.querySelector("#js-btn-scroll");
 
 // remplacer windows par le bouton, pour que le clic force la page à remonter
 
 btnScroll.addEventListener("scroll", function () {
-  navigation.classList.toggle("sticky", window.scrollY > 0);
+  nav.classList.toggle("sticky", window.scrollY > 0);
   window.scrollTo({
     top: 0,
     behavior: "smooth",
