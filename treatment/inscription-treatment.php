8996ee30a7084595d238dff2aba5f9fcddd57b50
William Caouette <william.caouette2001@gmail.com>
2021-05-04 09:51:10 -0400

    modification du traitement de l'inscription pour ajouter l'image

--- treatment/inscription-treatment.php
+++ treatment/inscription-treatment.php
@@ -10,7 +10,8 @@
         'password' => FILTER_SANITIZE_STRING,
         'pays' => FILTER_SANITIZE_STRING,
         'bio' => FILTER_SANITIZE_STRING,
-        'type' => FILTER_SANITIZE_STRING
+        'type' => FILTER_SANITIZE_STRING,
+        'image' => FILTER_SANITIZE_STRING
      ));
 
     //vérifie si le mail entrée est valide
@@ -26,14 +27,14 @@
             //sécurise le mot de passe entrée
             $motPasseSecurise = password_hash($utilsateur['password'], PASSWORD_BCRYPT);
 
-            $sth = $dbh->prepare("INSERT INTO `utilisateur`(`mail`, `password`, `location`, `bio`, `type` ) VALUES (:mail, :password, :location, :bio, :type);");
+            $sth = $dbh->prepare("INSERT INTO `utilisateur`(`mail`, `password`, `img`, `location`, `bio`, `type` ) VALUES (:mail, :password, :img, :location, :bio, :type);");
 
             $sth->bindParam(':mail', $utilsateur['mail'], PDO::PARAM_STR);
             $sth->bindParam(':password', $motPasseSecurise, PDO::PARAM_STR);
             $sth->bindParam(':location', $utilsateur['pays'], PDO::PARAM_STR);
             $sth->bindParam(':bio', $utilsateur['bio'], PDO::PARAM_STR);
             $sth->bindParam(':type', $utilsateur['type'], PDO::PARAM_STR);
-
+            $sth->bindParam(':img', $utilsateur['image'], PDO::PARAM_STR);
 
             if ($sth->execute()) {
                 header('Location: ../index.php');
