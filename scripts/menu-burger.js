2750f8bbe2fee2499308b7b90ff5113514f6028b
William Caouette <william.caouette2001@gmail.com>
2021-05-13 14:36:22 -0400

    modification du menu burger.js

--- scripts/menu-burger.js
+++ scripts/menu-burger.js
@@ -3,38 +3,12 @@
  * @author N.Prevel
  *
  * Created at     : 2021-04-25 19:32:12
- * Last modified  : 2021-04-29 08:43:17
+ * Last modified  : 2021-05-13 14:35:45
  */
-
-/*-- Menu burger --*/
-
-const boxes = document.querySelectorAll(".box");
-
-window.addEventListener("scroll", checkBoxesAppear);
-
-function checkBoxesAppear() {
-  boxes.forEach((box) => {
-    const boxTop = box.getBoundingClientRect().top;
-    const boxBottom = box.getBoundingClientRect().bottom;
-
-    const triggerPos = window.innerHeight - (boxBottom - boxTop) / 2;
-
-    if (boxTop < triggerPos) {
-      box.classList.add("show-box");
-    } else {
-      box.classList.remove("show-box");
-    }
-  });
-}
-
-checkBoxesAppear();
-
-/*-- Menu  --*/
 
 const btnBurger = document.querySelector(".fa-bars");
 const btnCancel = document.querySelector(".btn-cancel");
 const navigation = document.querySelector("nav");
-const mainContent = document.querySelector(".main-content");
 
 btnBurger.addEventListener("click", (evt) => {
   navigation.classList.add("nav-appear");
